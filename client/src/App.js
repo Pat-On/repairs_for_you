9a8afd5971fe1566683a61f9d1e3717519bca08d
SenaitEkubai <Senaitekubai@gmail.com>
2021-06-14 13:19:36 +0100

    created handypeople file and path

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import { Route, Switch } from "react-router-dom";
 
 import About from "./pages/About";
+import Handypeople from "./pages/Handypeople";
 import Home from "./pages/Home";
 
 const App = () => (
 	<Switch>
 		<Route path="/" exact><Home /></Route>
 		<Route path="/about/this/site"><About /></Route>
+		<Route path="/this/site/handyPeople"><Handypeople /></Route>
 	</Switch>
 );
 
---
+++ client/src/pages/Handypeople.js
@@ -0,0 +1,10 @@
+import React from "react";
+import HireFrom from "../handyPeopleList/HireFrom";
+
+export default function Handypeople() {
+	return (
+		<div>
+			<HireFrom />
+		</div>
+	);
+}

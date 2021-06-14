4ef0c43da32f41913482cbbff8986cf8a52ef1db
patryk <patryk.r.nowak@gmail.com>
2021-06-14 10:16:01 +0100

    connected layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import { Route, Switch } from "react-router-dom";
-
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Layout from "./hoc/Layout/Layout";
+
+import "./App.scss";
 
 const App = () => (
-	<Switch>
-		<Route path="/" exact><Home /></Route>
-		<Route path="/about/this/site"><About /></Route>
-	</Switch>
+	<div className="App container">
+		<Layout>
+			<Switch>
+				<Route path="/" exact component={About} />
+				<Route path="/about/this/site"><About /></Route>
+			</Switch>
+		</Layout>
+	</div>
 );
 
 export default App;

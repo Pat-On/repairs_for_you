e13c5e96b8e0b15080163741953f2019582ff9eb
SenaitEkubai <Senaitekubai@gmail.com>
2021-06-14 14:12:40 +0100

    created handypeople fram component

--- client/src/pages/Handypeople.js
+++ client/src/pages/Handypeople.js
@@ -1,10 +1,12 @@
 import React from "react";
+import HandyPeopleCard from "../handyPeopleList/handyPeopleCard";
 import HireFrom from "../handyPeopleList/HireFrom";
 
 export default function Handypeople() {
 	return (
 		<div>
 			<HireFrom />
+			<HandyPeopleCard />
 		</div>
 	);
 }

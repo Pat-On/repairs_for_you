efe8ea0c977b3d3471002934de7dfac9370fed46
patryk <patryk.r.nowak@gmail.com>
2021-06-14 15:13:25 +0100

    something

--- server/db.js
+++ server/db.js
@@ -4,6 +4,9 @@ const dbUrl = process.env.DATABASE_URL || "postgres://localhost:5432/cyf";
 
 const pool = new Pool({
 	connectionString: dbUrl,
+	ssl: {
+		rejectUnauthorized: false,
+	  },
 	connectionTimeoutMillis: 5000,
 });
 

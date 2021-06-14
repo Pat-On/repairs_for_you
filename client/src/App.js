0e5dbe3d4da8099bd76a65f72482108bcbae3bd0
patryk <patryk.r.nowak@gmail.com>
2021-06-14 10:49:44 +0100

    added placeholders

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,11 @@ const App = () => (
 	<div className="App container">
 		<Layout>
 			<Switch>
-				<Route path="/" exact component={About} />
+				<Route path="/" exact component={Home} />
+				<Route path="/buyers" exact component={() => <div><h1>PLACEHOLDER buyers</h1></div>} />
+				<Route path="/handy" exact component={() => <div><h1>PLACEHOLDER handy</h1></div>} />
+				<Route path="/contact" exact component={() => <div><h1>PLACEHOLDER CONTACTS</h1></div>} />
+
 				<Route path="/about/this/site"><About /></Route>
 			</Switch>
 		</Layout>

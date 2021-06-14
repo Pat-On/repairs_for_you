23b4bdf198959ca4676e677ffd3126bbd4aa970a
patryk <patryk.r.nowak@gmail.com>
2021-06-14 10:38:51 +0100

    added navigation

---
+++ client/src/hoc/Layout/Layout.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+import Aux from "../Auxillary/Auxillary";
+import Toolbar from "../../components/navigation/Toolbar/Toolbar";
+// import SideDrawer from "../../components/navigation/Sidedrwer/Sidedrawer";
+// import Footer from "../../components/Footer/Footer";
+
+import classes from "./Layout.module.scss";
+
+const Layout = (props) => {
+	//!TODO SIDE DRAWER
+	const [sideDrawerIsVisible, setSideDrawerVisible] = useState(false);
+
+	const sideDrawerClosedHandler = () => {
+		setSideDrawerVisible(false);
+	};
+	const sideDrawerToggleHandler = () => {
+		setSideDrawerVisible(!sideDrawerIsVisible);
+	};
+
+	return (
+		<Aux>
+			<Toolbar drawerToggleClicked={sideDrawerToggleHandler} />
+			{/* <SideDrawer open={sideDrawerIsVisible} closed={sideDrawerClosedHandler} /> */}
+
+			<main className={classes.content}>{props.children}</main>
+			{/* <Footer /> */}
+		</Aux>
+	);
+};
+
+export default Layout;

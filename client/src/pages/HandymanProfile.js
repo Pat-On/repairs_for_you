e3bf0be4ac72de5e0ffb02e378a66d6f3d634cd8
Michael <antenehnigussie1@yahoo.com>
2021-06-16 03:51:06 +0100

    Renamed file. Refactored code.

---
+++ client/src/pages/HandymanProfile.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import Handyman from "../components/Handyman/Handyman";
+
+const HandymanProfile = ({ id }) => {
+	const [user, setUser] = useState();
+	const [message, setMessage] = useState("Loading...");
+	useEffect(() => {
+		fetch(`/api/users/handyman/${id}`)
+			.then((res) => {
+				if (!res.ok) {
+					throw new Error(res.statusText);
+				}
+				return res.json();
+			})
+			.then((userData) => {
+				setUser(userData);
+			})
+			.catch((err) => {
+				console.error(err);
+				setMessage(err);
+			});
+	}, [id]);
+
+	return !user ? (
+		<h1 className="loading message-loading">{message}</h1>
+	) : (
+		<Handyman userData={user}/>
+	);
+};
+export default HandymanProfile;

c3fdb61ed698d70ef87be50ebc49d31e4fe72848
Michael <antenehnigussie1@yahoo.com>
2021-06-16 20:28:52 +0100

    Added handyman registration form (WIP).

---
+++ client/src/pages/RegistrationForm.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import HandymanRegistrationForm from "../components/HandymanRegistrationForm/HandymanRegistrationForm";
+
+const RegistrationForm = ({ formId }) => {
+  const [message, setMessage] = useState();
+  // EVENT HANDLERS
+  const getHandymanData = async (fromData) => {
+    const result = await sendRegistrationRequest(fromData);
+    setMessage(result.message);
+  };
+
+  return !formId ? (
+    <h1>Hello, world!</h1>
+  ) : formId === "handyman" ? (
+    <HandymanRegistrationForm onAddHandyman={getHandymanData} />
+  ) : (
+    <h1>Another Form</h1>
+  );
+};
+
+export default RegistrationForm;

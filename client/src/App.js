37045ab8436c970f7ac1e85650241c1f1ac83c9a
Michael <antenehnigussie1@yahoo.com>
2021-06-16 03:54:38 +0100

    Added router (/users/handyman).

--- client/src/App.js
+++ client/src/App.js
@@ -5,24 +5,53 @@ import Home from "./pages/Home";
 import Layout from "./hoc/Layout/Layout";
 
 import "./App.scss";
-
+import HandymanProfile from "./pages/HandymanProfile";
 
 const App = () => (
+  <div className="App container">
+    <Layout>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route
+          path="/buyers"
+          exact
+          component={() => (
+            <div>
+              <h1>PLACEHOLDER buyers</h1>
+            </div>
+          )}
+        />
+        <Route
+          path="/handy"
+          exact
+          component={() => (
+            <div>
+              <HandyPeople />{" "}
+            </div>
+          )}
+        />
 
-	<div className="App container">
-		<Layout>
-			<Switch>
-				<Route path="/" exact component={Home} />
-				<Route path="/buyers" exact component={() => <div><h1>PLACEHOLDER buyers</h1></div>} />
-				<Route path="/handy" exact component={() => <div><HandyPeople /> </div>} />
+        <Route
+          path="/contact"
+          exact
+          component={() => (
+            <div>
+              <h1>PLACEHOLDER CONTACTS</h1>
+            </div>
+          )}
+        />
 
-				<Route path="/contact" exact component={() => <div><h1>PLACEHOLDER CONTACTS</h1></div>} />
-
-				<Route path="/about/this/site"><About /></Route>
-
-			</Switch>
-		</Layout>
-	</div>
+        <Route path="/about/this/site">
+          <About />
+        </Route>
+        <Route
+          path="/handy/:id"
+          exact
+          render={({ match }) => <HandymanProfile id={match.params.id} />}
+        />
+      </Switch>
+    </Layout>
+  </div>
 );
 
 export default App;

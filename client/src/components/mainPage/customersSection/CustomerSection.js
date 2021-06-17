6e50e821b43e00bda7c1ef57f3807e0f7555499c
patryk <patryk.r.nowak@gmail.com>
2021-06-17 14:26:31 +0100

    styling of the customer comment from the main page -> next step figure

--- client/src/components/mainPage/customersSection/CustomerSection.js
+++ client/src/components/mainPage/customersSection/CustomerSection.js
@@ -20,6 +20,8 @@ const CustomerSection = (props) => {
       exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
       review: 5,
+      user_name: "Name1",
+      user_surname: "Surname1"
     },
     {
       photo: customer2,
@@ -28,6 +30,8 @@ const CustomerSection = (props) => {
     exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
      in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
       review: 3,
+      user_name: "Name2",
+      user_surname: "Surname2"
     },
     {
       photo: customer3,
@@ -36,6 +40,8 @@ const CustomerSection = (props) => {
   exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
    in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
       review: 4,
+      user_name: "Name3",
+      user_surname: "Surname3"
     },
   ];
 
@@ -60,8 +66,10 @@ const CustomerSection = (props) => {
           Your browser is not supported!
         </video>
       </div>
-      <h2 className={classes.customers__heading} > Happy Customers </h2>
-      <div className={classes.customers__commentContainer}> {usersComments} </div>
+      <h2 className={classes.customers__heading}> Happy Customers </h2>
+      <div className={classes.customers__commentContainer}>
+        {usersComments}
+      </div>
     </div>
   );
 };
--- client/src/components/mainPage/customersSection/customerCommentMain/CustomerCommentMain.js
+++ client/src/components/mainPage/customersSection/customerCommentMain/CustomerCommentMain.js
@@ -3,18 +3,20 @@ import { Route, Redirect, useRouteMatch, Switch, Link } from "react-router-dom";
 
 import classes from "./CustomerCommentMain.module.scss";
 
-
-
-
 const CustomerCommentMain = (props) => {
-
-  let stars = []
-for (let i = 0; i < props.item.review; i++ ) {
-  stars.push("   *   ")
-}
+  let stars = [];
+  for (let i = 0; i < props.item.review; i++) {
+    stars.push("   *   ");
+  }
   return (
     <div className={classes.customer}>
-      <img src={props.item.photo} />
+      <figure className={classes.customer__figure}>
+        <img src={props.item.photo} />
+        <figcaption className={classes.customer__figcaption}>
+          {props.item.user_name} {props.item.user_surname[0]}.
+        </figcaption>
+      </figure>
+
       <p>{props.item.comment}</p>
       <p>{stars}</p>
     </div>

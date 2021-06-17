83fc8ce65919987169030082e0d50125122400d4
patryk <patryk.r.nowak@gmail.com>
2021-06-17 12:02:32 +0100

    working video yupi

---
+++ client/src/components/mainPage/customersSection/CustomerSection.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import { Route, Redirect, useRouteMatch, Switch, Link } from "react-router-dom";
+
+import classes from "./CustomerSection.module.scss";
+
+import backgroundVideo from "../../../public/ConstructionWorkersDESKTOP.mp4"
+
+const CustomerSection = (props) => {
+  // !TODO: background video is good candidate for independent component
+
+  return (
+    <div className={classes.customers}>
+      <div className={classes.customers_bgVideo}>
+        <video className={classes.customers__bgVideo__video} autoPlay muted loop>
+          <source src={backgroundVideo} type="video/mp4" />
+          Your browser is not supported!
+        </video>
+      </div>
+    </div>
+  );
+};
+
+export default CustomerSection;
--- client/src/components/mainPage/mainPage.js
+++ client/src/components/mainPage/mainPage.js
@@ -4,16 +4,16 @@ import { Route, Redirect, useRouteMatch, Switch, Link } from "react-router-dom";
 import classes from "./MainPage.module.scss";
 import PrimarySection from "./primarySection/PrimarySection";
 import AboutSection from "./aboutSection/AboutSection";
+import CustomersSection from "./customersSection/CustomerSection";
 
 // !TODO: decomposition of this elements in next stage
-
 
 const MainPage = (props) => {
   return (
     <div className={classes.mainPage}>
       <PrimarySection />
       <AboutSection />
-      <div className={classes.mainPage__customers}>Customer</div>
+      <CustomersSection />
       <div className={classes.mainPage__explanation}>Explanation</div>
     </div>
   );

14b0a9dd2dc29c88d58bed1fd2a8ca9751f7097b
patryk <patryk.r.nowak@gmail.com>
2021-06-18 20:23:32 +0100

    modificitation to sign in

--- client/src/containers/signIn/SignIn.js
+++ client/src/containers/signIn/SignIn.js
@@ -6,18 +6,9 @@ import CompletionFormScreen from "../../components/completionFormScreen/Completi
 
 import { checkValidity } from "../../utility/utility";
 
-const FIRST_FORM_PAGE = 0;
-const SECOND_FORM_PAGE = 1;
-const THIRD_FORM_PAGE = 2;
-const LAST_FORM_PAGE = 3;
-const NAME_AND_PHONE_NUMBER_SLICE_INDEX = [0, 2];
-const EMAIL_AND_DATE_OF_BIRTH_SLICE_INDEX = [2, 4];
+
 
 const SignIn = () => {
-  /**
-   * State: pageControl and setPageControl are used to managing the displayed page of form
-   */
-  const [pageControl, setPageControl] = useState(FIRST_FORM_PAGE);
 
   /**
    * State: responsible for controlling the process of displaying the http req in UI
@@ -30,32 +21,6 @@ const SignIn = () => {
    *  -storing the input from user
    */
   const [signForm, setSignForm] = useState({
-    name: {
-      invalidInputInfo: "Have to contains only letters",
-      name: "Name",
-      placeholder: "Name",
-      value: "",
-
-      validation: {
-        required: true,
-        isName: true,
-      },
-      valid: false,
-      touched: false,
-    },
-    number: {
-      invalidInputInfo: "UK phone number - 11 digits",
-      name: "Phone Number",
-      placeholder: "01234567890",
-      value: "",
-
-      validation: {
-        required: true,
-        isTelNumber: true,
-      },
-      valid: false,
-      touched: false,
-    },
     email: {
       invalidInputInfo: "Enter valid email address",
       name: "E-mail",
@@ -69,16 +34,15 @@ const SignIn = () => {
       valid: false,
       touched: false,
     },
-    dateOfBirth: {
-      invalidInputInfo: "You must be 18+. DD/MM/YYYY",
-      name: "Date of birth",
-      placeholder: "dd/mm/yyyy",
+    password: {
+      invalidInputInfo: "Have to be at least 8 symbols long",
+      name: "Password",
+      placeholder: "Password",
       value: "",
 
       validation: {
         required: true,
-        isDOB: true,
-        isEighteen: true,
+        isName: true,
       },
       valid: false,
       touched: false,
@@ -93,11 +57,7 @@ const SignIn = () => {
    * @return nothing
    */
   const backFunction = () => {
-    setPageControl(() => {
-      if (pageControl === FIRST_FORM_PAGE) return FIRST_FORM_PAGE;
-      const page = pageControl;
-      return page - 1;
-    });
+    console.log("Placeholder");
   };
 
   /**
@@ -108,53 +68,7 @@ const SignIn = () => {
    * @return nothing
    */
   const nextFunction = () => {
-    setPageControl(() => {
-      if (pageControl === LAST_FORM_PAGE) return LAST_FORM_PAGE;
-      const page = pageControl;
-      return page + 1;
-    });
-  };
-
-  /**
-   * @Description Function showing to user that data is sending to the server
-   * FUNCTION IN THAT FORM FAKING SENDING DATA by using setTimeout()
-   *
-   * After setTimeout() passed function is triggering rendering the LAST_FORM_PAGE
-   * @input no input
-   * @return nothing
-   */
-  const confirmation = () => {
-    setLoadingControl(true);
-    setTimeout(() => {
-      setPageControl(LAST_FORM_PAGE);
-      setLoadingControl(false);
-    }, 800);
-  };
-
-  /**
-   * @Description  Function returning to main page, in that case to FIRST_FORM_PAGE of form
-   * Function is changing the page by changing the state of loadingControl by setLoadingControl
-   *
-   * Function is cleaning previously provided by user data stored in APP
-   * @input no input
-   * @return nothing
-   */
-
-  const returnToMain = () => {
-    setPageControl(FIRST_FORM_PAGE);
-    let newState = { ...signForm };
-    for (let key of Object.keys(signForm)) {
-      newState = {
-        ...newState,
-        [key]: {
-          ...newState[key],
-          value: "",
-          touched: false,
-          valid: false,
-        },
-      };
-    }
-    setSignForm(newState);
+    console.log("Placeholder");
   };
 
   /**
@@ -189,50 +103,16 @@ const SignIn = () => {
   }
 
   let form = "";
-  /**
-   *  Switch statement which base on pageControl value rendering different pages of the form
-   */
-  switch (pageControl) {
-    case FIRST_FORM_PAGE:
-      form = (
-        <SignUpForm
-          form={formElementKeyArray.slice(...NAME_AND_PHONE_NUMBER_SLICE_INDEX)}
-          formInputHandler={inputChangeHandler}
-          next={nextFunction}
-          buttonDisable={!(signForm.name.valid && signForm.number.valid)}
-        />
-      );
-      break;
-    case SECOND_FORM_PAGE:
-      form = (
-        <SignUpForm
-          form={formElementKeyArray.slice(
-            ...EMAIL_AND_DATE_OF_BIRTH_SLICE_INDEX
-          )}
-          formInputHandler={inputChangeHandler}
-          back={backFunction}
-          next={nextFunction}
-          buttonDisable={!(signForm.email.valid && signForm.dateOfBirth.valid)}
-        />
-      );
-      break;
-    case THIRD_FORM_PAGE:
-      form = (
-        <ConfirmationFormPage
-          loadingControl={loadingControl}
-          back={backFunction}
-          next={confirmation}
-          formValues={signForm}
-        />
-      );
-      break;
-    case LAST_FORM_PAGE:
-      form = <CompletionFormScreen back={returnToMain} />;
-      break;
-    default:
-      console.log("You should never see that");
-      break;
-  }
+
+  form = (
+    <SignUpForm
+      form={formElementKeyArray}
+      formInputHandler={inputChangeHandler}
+      next={nextFunction}
+      back={backFunction}
+      buttonDisable={!(signForm.password.valid && signForm.email.valid)}
+    />
+  );
 
   return <div>{form}</div>;
 };

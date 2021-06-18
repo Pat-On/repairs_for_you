0ab79731f7cf4068a7cab0b2f1a3f7c1c9a1878c
patryk <patryk.r.nowak@gmail.com>
2021-06-18 10:20:24 +0100

    explanation subpage to main page - temporary solution

--- client/src/components/mainPage/explanation/explanation.js
+++ client/src/components/mainPage/explanation/explanation.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import classes from "./explanation.module.scss";
 
+import svgArrow from "../../../public/right-arrow.svg"
+
 /**
  * @DescriptionFunction Sub component used in MainPage component
  * !TODO: component is not yet implemented it is just placeholder
@@ -13,16 +15,20 @@ const Explanation = (props) => {
 
       <h3 className={classes.explanation__header__sub__1}> For Buyers: </h3>
 	  <div className={`${classes.explanation__flexContainer} ${classes.explanation__listGridPos1}`}>
-      <p className={classes.explanation__p1}>first step</p>
-      <p className={classes.explanation__p2}>second step</p>
-      <p className={classes.explanation__p3}>this step</p>
+      <p className={classes.explanation__p1}>Register by email Your request</p>
+	  <img className={classes.explanation__arrowSVG} src={svgArrow} alt="Arrow icon made by Roundicons" />
+      <p className={classes.explanation__p2}>Receive offer</p>
+	  <img className={classes.explanation__arrowSVG} src={svgArrow} alt="Arrow icon made by Roundicons" />
+      <p className={classes.explanation__p3}>Get Service From US</p>
 	  </div>
 
       <h3 className={classes.explanation__header__sub__2}> For Handyperson: </h3>
 	  <div className={`${classes.explanation__flexContainer} ${classes.explanation__listGridPos2}`}>
-      <p className={classes.explanation__p1}>first step</p>
-      <p className={classes.explanation__p2}>second step</p>
-      <p className={classes.explanation__p3}>this step</p>
+      <p className={classes.explanation__p1}>Register Your Service</p>
+	  <img className={classes.explanation__arrowSVG} src={svgArrow} alt="Arrow icon made by Roundicons" />
+      <p className={classes.explanation__p2}>Get Acceptation</p>
+	  <img className={classes.explanation__arrowSVG} src={svgArrow} alt="Arrow icon made by Roundicons" />
+      <p className={classes.explanation__p3}>Receive Job Offers</p>
 	  </div>
 
     </div>

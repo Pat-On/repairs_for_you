8d05ce5f99750ea2cf8058e7d7b736b7e5533171
Michael <antenehnigussie1@yahoo.com>
2021-06-20 09:56:05 +0100

    Improved form layout.

--- client/src/components/Handyman/RequestForQuoteForm/RequestForQuoteForm.js
+++ client/src/components/Handyman/RequestForQuoteForm/RequestForQuoteForm.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import "./RequestForQuoteForm.css";
 import Skills from "../SubComponents/Skills";
+import { validateForm } from "../functions";
 
-const RequestForQuoteForm = () => {
+const RequestForQuoteForm = (props) => {
+  const data = props.data.location.state;
+  console.log(data.skills);
   const [errors, setErrors] = useState([]);
   // EVENT HANDLERS
   const sendFormData = (event) => {
@@ -12,45 +15,61 @@ const RequestForQuoteForm = () => {
       return setErrors(errors);
     }
     setErrors([]);
-    alert("Thank you, your request has been sent. We will get back to you as soon as possible");
-    window.location.reload();
+    alert(
+      "Thank you, your request has been sent. We will get back to you as soon as possible"
+    );
   };
 
   return (
-    <form id="form-add-quote" onSubmit={sendFormData}>
+    <form id="form-add-quote" className="form" onSubmit={sendFormData}>
       {errors.map((e, index) => (
         <p key={index} className="error">
           {e}
         </p>
       ))}
       <h1>Request For Quote</h1>
+      <em className="required">
+        <span className="required">*</span>&nbsp;Required field
+      </em>
+      <div className="">
+        <h2>Handyman Details</h2>
+        <p>
+          Handyman Name: <span>{data.firstName}</span>{" "}
+          <span>{data.lastName}</span>
+        </p>
+        <p>
+          Area: <span>{data.address.area}</span>
+        </p>
+      </div>
       <div>
-        <div className="input-field">
-          <label htmlFor="first-name">Your Name</label>
-          <input
-            type="text"
-            id="buyer-name"
-            name="form-add-quote"
-            max={60}
-            required
-            placeholder="Enter your name here"
-          />
-        </div>
-        <div className="input-field-group">
-          <h2 className="subtile">Contact Details</h2>
+        <fieldset className="input-field-group job-details">
+          <legend className="subtitle">Contact Details</legend>
           <div className="input-field">
-            <label htmlFor="email">Email</label>
+            <label htmlFor="first-name">Your Name</label>
+            <span className="required">*</span>&nbsp;
+            <input
+              type="text"
+              id="buyer-name"
+              name="form-add-quote"
+              max={60}
+              // required
+              placeholder="Enter your name here"
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="email">Your Email</label>
+            <span className="required">*</span>&nbsp;
             <input
               type="email"
               id="email"
               name="form-add-quote"
               max={50}
-              required
+              // required
               placeholder="someone@example.com"
             />
           </div>
           <div className="input-field">
-            <label htmlFor="phone-number">Phone Number (optional)</label>
+            <label htmlFor="phone-number">Phone Number (optional)</label>{" "}
             <input
               type="tel"
               id="phone-number"
@@ -58,47 +77,60 @@ const RequestForQuoteForm = () => {
               max={50}
             />
           </div>
-        </div>
+        </fieldset>
       </div>
-      <Skills formId="form-add-quote" />
-      <div className="input-field-group">
-        <h2 htmlFor="subtitle">Job Details</h2>
-        <div className="input-field">
-          <textarea
-            id="job-description"
-            name="form-add-quote"
-            max={1000}
-            required
-            placeholder="Short summary of the job"
-          ></textarea>
+      <fieldset className="input-field-group job-details">
+        <legend className="subtitle">Job Details</legend>
+        <div className="input-field-group">
+          <div className="input-field">
+            <h3>
+              Job Description
+              <span className="required">*</span>&nbsp;
+            </h3>
+            <textarea
+              id="job-description"
+              name="form-add-quote"
+              max={1000}
+              // required
+              placeholder="Short summary of the job"
+            ></textarea>
+          </div>
+          <div className="handyman-skills">
+            <h3>Required Skill(s) (optional)</h3>
+            <Skills skills={data.skills} formId="form-add-quote" />
+          </div>
+          <div className="input-field">
+            <label htmlFor="date-start">Expected Start Date</label>
+            <span className="required">*</span>&nbsp;
+            <input
+              type="date"
+              id="date-start"
+              name="form-add-quote"
+              max={new Date().toLocaleDateString()}
+              // required
+            />
+          </div>
+          <div>
+            <em className="required">
+              Please provide estimate for at least one of the following:
+            </em>
+            <div className="input-field">
+              <label htmlFor="job-duration-days">
+                Estimated Number of Hours:
+              </label>{" "}
+              <input
+                type="number"
+                id="job-duration-days"
+                name="form-add-quote"
+              />
+            </div>
+            <div className="input-field">
+              <label htmlFor="price">I am willing to pay £</label>{" "}
+              <input type="number" id="price" name="form-add-quote" />
+            </div>
+          </div>
         </div>
-        <div className="input-field">
-          <label htmlFor="date-start">Expected for Date:</label>{" "}
-          <input
-            type="date"
-            id="date-start"
-            name="form-add-quote"
-            max={new Date().toLocaleDateString()}
-            required
-          />
-        </div>
-        <div className="input-field">
-          <label htmlFor="job-duration-days">Estimated Number of Hours:</label>{" "}
-          <input
-            type="number"
-            id="job-duration-days"
-            name="form-add-quote"
-          />
-        </div>
-        <div className="input-field">
-          <label htmlFor="price">I am willing to pay £</label>{" "}
-          <input
-            type="number"
-            id="price"
-            name="form-add-quote"
-          />
-        </div>
-      </div>
+      </fieldset>
       <div className="submit-button-div">
         <input
           type="submit"

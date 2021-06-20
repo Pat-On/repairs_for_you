187c3613671e5a502b7e58316594f801eb5c37f2
Michael <antenehnigussie1@yahoo.com>
2021-06-20 10:04:33 +0100

    Updated component import and properties.

--- client/src/pages/RequestForQuote.js
+++ client/src/pages/RequestForQuote.js
@@ -1,15 +1,16 @@
 import { useState } from "react";
-import RequestForQuoteForm from "../components/RequestForQuoteForm/RequestForQuoteForm";
+import RequestForQuoteForm from "../components/Handyman/RequestForQuoteForm/RequestForQuoteForm";
 
-const RequestForQuote = () => {
+const RequestForQuote = (props) => {
   const [message, setMessage] = useState();
+  console.log(props)
   // EVENT HANDLERS
   const getRequestForQuoteData = async (fromData) => {
     const result = await sendRegistrationRequest(fromData);
     setMessage(result.message);
   };
 
-  return <RequestForQuoteForm onAddHandyman={getRequestForQuoteData} />;
+  return <RequestForQuoteForm data={props} onSubmit={getRequestForQuoteData} />;
 };
 
 export default RequestForQuote;

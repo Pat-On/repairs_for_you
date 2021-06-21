c8df5d9e335b4440789e1fc156930abec436593e
Michael <antenehnigussie1@yahoo.com>
2021-06-21 18:00:52 +0100

    Added more functions.

--- client/src/common/js/functions.js
+++ client/src/common/js/functions.js
@@ -1,4 +1,5 @@
 import emailValidator from "email-validator";
+import { send } from "emailjs-com";
 
 export function validateForm(form) {
   const id = form.id;
@@ -50,8 +51,123 @@ function validatePhoneNumuber(phoneNumber) {
   return phoneNumber.every((num) => Number(num));
 }
 
-export function sendQuoteRequest(requestData) {
-  alert(
-    "Thank you for choosing us! Your request has been sent. We will get back to you soon."
-  );
+// SEND QUOTE REQUEST EMAIL TO ADMIN
+export function sendQuoteRequest(formData) {
+  return formData
+    ? alert(
+        "Thank you for choosing us! Your request has been sent. We will get back to you soon."
+      )
+    : alert(
+        "Sorry, but we couldn't send your request. Please try again later."
+      );
+}
+
+// SEND NEW HANDYMAN REGISTRATION REQUEST EMAIL TO ADMIN
+export function sendRegistrationRequest(requestData) {
+  const [service, template, form, user] = requestData;
+  const formDataObject = getRegistrationFormDataObject(form);
+  sendEmailToAdmin([service, template, formDataObject, user]);
+}
+
+// THIS IS WHERE EMAILS ARE SENT FROM
+function sendEmailToAdmin([...args]) {
+  const [service, template, formDataEntries, user] = args;
+
+  send(service, template, formDataEntries, user)
+    .then((response) => {
+      alert(
+        "Thank you for choosing us! Your request has been sent. We will get back to you soon."
+      );
+      console.log("SUCCESS!", response.status, response.text);
+    })
+    .catch((err) => {
+      console.log("FAILED...", err);
+    });
+}
+
+// COLLECT ALL HANDYMAN REGISTRATION FORM INPUT AND CHANGE IT TO AN OBJECT TO BE SENT VIA HTTP REQUEST
+function getRegistrationFormDataObject(form) {
+  const formDataDataObject = {};
+  const contact = {};
+  const address = {};
+  const skills = {};
+
+  // BASIC DETAILS
+  const [firstName, lastName] = [
+    ...form.querySelector(".basic-details").querySelectorAll("input"),
+  ].map((item) => item.value);
+
+  // ADDRESS
+  const addressData = [
+    ...form.querySelector(".address-details").querySelectorAll("input"),
+  ].map((item) => [item.id, item.value]);
+  addressData.forEach((item) => (address[item[0]] = item[1]));
+
+  const contactData = [
+    ...form.querySelector(".contact-details").querySelectorAll("input"),
+  ].map((item) => [item.id, item.value]);
+  contactData.forEach((item) => (contact[item[0]] = item[1]));
+
+  // SKILLS
+  const skillsData = [
+    ...form
+      .querySelector(".skills-list")
+      .querySelectorAll("input[type='checkbox']"),
+  ]
+    .filter((skill) => skill.checked)
+    .map((item) => [item.id, item.value]);
+  skillsData.forEach((item) => (skills[item[0]] = item[1]));
+
+  // BIO
+  const bio = form.querySelector("#bio").value;
+
+  formDataDataObject.bio = bio;
+  formDataDataObject.skills = skills;
+  formDataDataObject.contact = contact;
+  formDataDataObject.address = address;
+  formDataDataObject.lastName = lastName;
+  formDataDataObject.firstName = firstName;
+
+  return formDataDataObject;
+}
+
+// COLLECT ALL HANDYMAN REGISTRATION FORM INPUT AND CHANGE IT TO AN OBJECT TO BE SENT VIA HTTP REQUEST
+function getRequestForQuoteFormDataObject(form) {
+  const formDataDataObject = {};
+  const contact = {};
+  const address = {};
+  const skills = {};
+  // BASIC DETAILS
+  const [firstName, lastName] = [
+    ...form.querySelector(".basic-details").querySelectorAll("input"),
+  ].map((item) => item.value);
+
+  // ADDRESS
+  const addressData = [
+    ...form.querySelector(".address-details").querySelectorAll("input"),
+  ].map((item) => [item.id, item.value]);
+  addressData.forEach((item) => (address[item[0]] = item[1]));
+  console.log(addressData);
+  const contactData = [
+    ...form.querySelector(".contact-details").querySelectorAll("input"),
+  ].map((item) => [item.id, item.value]);
+  contactData.forEach((item) => (contact[item[0]] = item[1]));
+
+  // SKILLS
+  const skillsData = [
+    ...form
+      .querySelector(".skills-list")
+      .querySelectorAll("input[type='checkbox']"),
+  ]
+    .filter((skill) => skill.checked)
+    .map((item) => [item.id, item.value]);
+  skillsData.forEach((item) => (skills[item[0]] = item[1]));
+
+  formDataDataObject.skills = skills;
+  formDataDataObject.contact = contact;
+  formDataDataObject.address = address;
+  formDataDataObject.lastName = lastName;
+  formDataDataObject.firstName = firstName;
+
+  return formDataDataObject;
 }

88155987a397bc3a6de409355b580c88b09e134d
patryk <patryk.r.nowak@gmail.com>
2021-06-22 18:33:52 +0100

    added temporary logout component

---
+++ client/src/containers/signOut/signOut.js
@@ -0,0 +1,15 @@
+import React, { useState, useContext, useEffect } from "react";
+import AuthContext from "../../store/authContext";
+
+const SignOut = (props) => {
+  const authCtx = useContext(AuthContext);
+
+  useEffect(() => {
+    authCtx.logout();
+    props.history.push("/");
+  }, []);
+
+  return <div>WWe can add here loading spinner</div>;
+};
+
+export default SignOut;

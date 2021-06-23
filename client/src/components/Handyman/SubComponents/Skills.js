f3bbfd16d7856115d06f69b2a03ebe4f02abd0b3
Michael <antenehnigussie1@yahoo.com>
2021-06-23 10:25:21 +0100

    Made the component "stateful". Updated code.

--- client/src/components/Handyman/SubComponents/Skills.js
+++ client/src/components/Handyman/SubComponents/Skills.js
@@ -1,15 +1,39 @@
-const Skills = ({ skills }) => {
+import { useState } from "react";
+
+const Skills = (props) => {
+  const unselectedSkillsList = props.skills.map((skill) => ({
+    name: skill.name,
+    value: "",
+  }));
+
+  const [updatedSkillsList, setUpdatedSkillsList] =
+    useState(unselectedSkillsList);
+
+  const handleChange = (event) => {
+    const skill = event.target;
+    const newList = updatedSkillsList.map((item) => {
+      if (item.name === skill.name && skill.checked) {
+        return { ...item, value: skill.value };
+      }
+      return { ...item, value: item.value };
+    });
+    setUpdatedSkillsList(newList);
+    // now send only the selected skills
+    props.onChangeHandler(newList);
+  };
+
   return (
     <div className="skills-list">
-      {skills.map((skill, index) => (
+      {props.skills.map((skill, index) => (
         <div key={index} className="input-field">
           <input
             type="checkbox"
             id={skill.id}
             name={skill.name}
             value={skill.value}
+            onChange={handleChange}
           />{" "}
-          <label htmlFor="carpentry">{skill.name}</label>
+          <label htmlFor="carpentry">{skill.value}</label>
         </div>
       ))}
     </div>

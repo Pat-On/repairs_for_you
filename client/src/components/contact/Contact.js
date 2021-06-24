cbc7045df8a3a42b8ec419c1a235234b89f34cba
SenaitEkubai <Senaitekubai@gmail.com>
2021-06-24 22:15:59 +0100

    contact us form submission

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Contact.css";
-import MapComponent from "./MapComponent";
 import validateForm from "../../common/js/functions";
 import { permittedCrossDomainPolicies } from "helmet";
+import { send } from "emailjs-com";
 
 export default function Contact() {
 	const [formDetail,setFormDetail]=useState({
@@ -14,16 +14,22 @@ export default function Contact() {
 
 	const handleChange=(e)=>{
 		e.preventDefault();
-		setFormDetail({ ...[formDetail],[e.target.name]:e.target.value });/* not completed  */
+		setFormDetail({
+			...formDetail,
+			[e.target.name]: e.target.value,
+		});
 	};
 	const handleSubmit=(e)=>{
 		e.preventDefault();
 		console.log(formDetail);
+		send( "service_l0m5rpd",
+			"template_ybb8yxc",
+			formDetail,
+			"user_Z6650OqueHooRxmmi5Geo",);
 	};
+
 	return (
 		<div className="contact-container">
-
-
 			<div className="map-form" onSubmit={handleSubmit}>
 				<form
 					id="form-send-quote"
@@ -31,70 +37,77 @@ export default function Contact() {
 					className="form"
 				>
 					<div>
-						<h1>Contact Us</h1>
-						<fieldset>
-							<span>Got a question, we would love to hear from you.</span>
-							<span>  Send us a message and we will respond as soon as possible</span>
-							<div className="input-fields-container">
-								<div className="input-field">
-									<label>Name: {" "}</label>
-									<input
-										type="text"
-										id="buyer_name"
-										name="buyer_name"
-										maxLength={60}
-										required
-										placeholder="Enter your name here"
-										onChange={handleChange}
-									/>
-								</div>
-								<div className="input-field">
-									<label>Email Address: {" "}</label>
-									<input
-										type="text"
-										id="email"
-										name="email"
-										maxLength={60}
-										required
-										placeholder="Enter your email here"
-										onChange={handleChange}
+						<div className="contact-text">
+							<h1>Contact Us</h1>
+							<p>Got a question, we would love to hear from you.</p>
+							<p>Drop us a message and one of our team members will get in touch with you</p>
 
+						</div>
+						<div className="contact-info">
+							<p>Tele-+4470000000</p>
+							<p>repairsyou@gmail.com</p>
+							<a href="https://goo.gl/maps/KaWYzgRdgEwHb9xHA">Coventry refugee and migrant center</a>
+						</div>
 
-									/>
-								</div>
-								<div className="message">
-									<label> Message: {" "}</label>
-									<textarea
-										type="text"
-										id="message"
-										name="message"
-										maxLength={60}
-										required
-										placeholder="Enter your message here"
-										onChange={handleChange}
-
-									/>
-								</div>
+						<div className="input-fields-container">
+							<div className="input-field">
+								<label>Name: {" "}</label>
+								<input
+									type="text"
+									id="buyer_name"
+									name="buyer_name"
+									maxLength={60}
+									required
+									placeholder="Enter your name here"
+									onChange={handleChange}
+								/>
 							</div>
+							<div className="input-field">
+								<label>Email Address: {" "}</label>
+								<input
+									type="text"
+									id="email"
+									name="email"
+									maxLength={60}
+									required
+									placeholder="Enter your email here"
+									onChange={handleChange}
 
-						</fieldset>
+
+								/>
+							</div>
+							<div className="message">
+								<label> Message: {" "}</label><span><textarea
+									type="text"
+									id="message"
+									name="message"
+									required
+									placeholder="Enter your message here"
+									onChange={handleChange}
+
+								/></span>
+
+							</div>
+						</div>
+
+
 
 						<div className="input-field">
 							<input
 								className="btn-submit"
 								type="submit"
 								id="buyer_name"
-								name="buyer_name"
+								name="submit"
 								maxLength={60}
 								required
-								placeholder="Enter your name here"
+								placeholder="Enter your message here"
 
 							/>
 						</div>
 
 					</div>
 				</form>
-				<MapComponent />
+
 
 			</div>
 			<Link to="/contact"></Link>

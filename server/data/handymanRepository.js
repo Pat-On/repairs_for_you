4d59be18b99128e62edcdda6600f1b4da22d4208
Michael <antenehnigussie1@yahoo.com>
2021-06-24 17:52:50 +0100

    Added a POST request handler.

--- server/data/handymanRepository.js
+++ server/data/handymanRepository.js
@@ -1,16 +1,49 @@
-import handymen from "../../HandyPeopleTestData.json"
+import handymen from "../../HandyPeopleTestData.json";
 
 // get all handymen
 function getAllHandymen() {
-  return [...handymen]
+  return [...handymen];
 }
 
 // search/find a handyman from list by video id
 function getHandymanById(hId) {
-  return handymen.find(hMan => hMan.id === hId);
+  return handymen.find((hMan) => hMan.id === hId);
+}
+
+function getHandymanByEmail(email) {
+  return handymen.find((hMan) => hMan.email === email);
+}
+
+// add handyman to list
+function addNewHandyman(hData) {
+  const {
+    firstName,
+    lastName,
+    img,
+    address,
+    postcode,
+    email,
+    phoneNumber,
+    skills,
+    bio,
+  } = hData;
+
+  return handymen.push({
+    firstName,
+    lastName,
+    img,
+    address,
+    postcode,
+    email,
+    phoneNumber,
+    skills,
+    bio,
+  });
 }
 
 module.exports = {
   getAllHandymen,
   getHandymanById,
+  getHandymanByEmail,
+  addNewHandyman,
 };
--- server/services/handymanServices.js
+++ server/services/handymanServices.js
@@ -11,7 +11,83 @@ async function getHandymanById(hId) {
   return result;
 }
 
+// add handyman to list
+async function addNewHandyman(hData) {
+  const dataIsValid = validateHandymanData(hData);
+  if (dataIsValid) {
+    try {
+      const handymanIsNew = await handymanDoesntExist(hData.email);
+      if (handymanIsNew) {
+        await repository.addNewHandyman(hData);
+        return {
+          status: "OK",
+          message: "User has been added successfully.",
+        };
+      }
+      return {
+        status: "FAIL",
+        message:
+          "Sorry, but we could not send your request. A user with the same email already exists.",
+      };
+    } catch (error) {
+      // if there is database connection issue
+      return console.log(error);
+    }
+  }
+  return {
+    status: "FAIL",
+    message: "Handyman could not be saved. Missing handyman information.",
+  };
+}
+
+// validate incoming handyman data
+function validateHandymanData(hData) {
+  // required handyman data fields
+  try {
+    const {
+      firstName,
+      lastName,
+      address,
+      postcode,
+      email,
+      phoneNumber,
+      skills,
+      bio,
+    } = hData;
+    // destructure address (={addressLineOne, addressLineTwo,city}) and access individual field values
+    // city has default value of "Coventry", so no need to validate that
+    const { addressLineOne, addressLineTwo } = address;
+    
+    const dataToValidate = {
+      firstName,
+      lastName,
+      addressLineOne,
+      addressLineTwo,
+      postcode,
+      email,
+      phoneNumber,
+      skills,
+      bio,
+    };
+    
+    return Object.values(dataToValidate).every((item) => item);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// check existence of a handyman before posting
+async function handymanDoesntExist(hEmail) {
+  try {
+    const result = await repository.getHandymanByEmail(hEmail);
+    return result === undefined;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 module.exports = {
   getAllHandymen,
   getHandymanById,
+  addNewHandyman,
 };

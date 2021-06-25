48b9af8f5aa1ca993f8cb1e31e00f0c5d1a542a8
Michael <antenehnigussie1@yahoo.com>
2021-06-25 17:53:46 +0100

    Updated code. Connected to database.

--- server/data/quotesRepository.js
+++ server/data/quotesRepository.js
@@ -1,41 +1,44 @@
-const quotes = [];
+import pool from "../db";
+
+// QUERIES
+
+const getAllQuotesQuery = `SELECT * FROM quotes`; //**WARN: NOT IMPLEMENTED YET**
+const getQuoteByIdQuery = `SELECT * FROM handymen WHERE id = $1`; // **WARN: NOT IMPLEMENTED YET**
+const addNewQuoteQuery = `
+INSERT INTO quotes (client_name, client_email, job_description, job_start_date, price, handyman_id)
+  VALUES($1, $2, $3, $4, $5, $6)`;
 
 // FETCH DATA OF ALL QUOTES FROM DATABASE BY QUOTE ID (currently accessible only to admin)
-// WARN: NOT CONNECTED TO DATABASE YET!
 function getAllQuotes() {
-  return [...quotes];
+  return pool.query(getAllQuotesQuery);
 }
 
 // FETCH DATA OF ALL QUOTES FROM DATABASE BY QUOTE ID (currently accessible only to admin)
-// WARN: NOT CONNECTED TO DATABASE YET!
 function getQuoteById(qId) {
-  return quotes.find((quote) => quote.id === qId);
+  return pool.query(getQuoteByIdQuery, [qId]);
 }
 
 // STORE QUOTE DATA IN THE DATABASE (quotes table)
-// WARN: NOT CONNECTED TO DATABASE YET!
-// Note: this is used only during the initial stage of quote process (accessible to anyone... 
+// Note: this is used only during the initial stage of quote process (accessible to anyone...
 // ...who would like get quotes from a selected handyman on the site)
 function addNewQuote(qData) {
   const {
     buyerName,
     buyerEmail,
-    buyerPhoneNumber,
     jobDescription,
     jobStartDate,
     estimatedManHours,
     handymanId,
   } = qData;
 
-  return quotes.push({
+  return pool.query(addNewQuoteQuery, [
     buyerName,
     buyerEmail,
-    buyerPhoneNumber,
     jobDescription,
     jobStartDate,
     estimatedManHours,
     handymanId,
-  });
+  ]);
 }
 
 module.exports = {

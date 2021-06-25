65cb263c7b2931c87723228869600b90a2f27fd2
patryk <patryk.r.nowak@gmail.com>
2021-06-25 21:26:05 +0100

    to finish - input form bolean type logic

--- client/src/components/adminPanel/usersManagements/temporaryEditForm/temporaryEditForm.js
+++ client/src/components/adminPanel/usersManagements/temporaryEditForm/temporaryEditForm.js
@@ -32,10 +32,17 @@ const TemporaryEditForm = (props) => {
       });
   }, []);
 
-const handleChange = (e) => {
-console.log(e.target)
-}
+  const handleChange = (e) => {
+    console.log(e.target.value);
 
+    //TODO: modify it to handle the update of the visibility true / false
+    setUser((prevState) => {
+      return {
+        ...prevState,
+        [`${e.target.name}`]: e.target.value,
+      };
+    });
+  };
 
   // Function to send update
   const patchUser = async () => {
@@ -48,13 +55,35 @@ console.log(e.target)
   //"form" to input the data to sent if needed
 
   // TODO: disabled='disabled'
-console.log(user.first_name)
-  return (
-    <form>
-      <p>First name:</p>
-      <input type="text" id="first_name" name="fname" value={user.first_name}  onChange={handleChange}/>
-    </form>
-  );
+  console.log(user);
+
+  let form = <p>LOADING...</p>;
+  if (user.first_name) {
+    form = (
+      <form>
+        <p>First name:</p>
+        <input
+          type="text"
+          id="first_name"
+          name="first_name"
+          value={user.first_name}
+          onChange={handleChange}
+        />
+              <p>First name:</p>
+        <input
+          type="text"
+          id="last_name"
+          name="last_name"
+          value={user.last_name}
+          onChange={handleChange}
+        />
+        <p>Visible /Not visible</p>
+        <input type="checkbox" name="inputOne" onClick={handleChange} />
+      </form>
+    );
+  }
+
+  return form;
 };
 
 export default TemporaryEditForm;

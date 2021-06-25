b3c5f2b7b07c03531d3aee1c389660743c595a50
Michael <antenehnigussie1@yahoo.com>
2021-06-25 12:03:00 +0100

    Edited comment lines. Added clarification notes.

--- server/routers/handymanRouter.js
+++ server/routers/handymanRouter.js
@@ -6,19 +6,21 @@ const services = require("../services/handymanServices");
 
 router.use(express.json());
 
-// GET "/"
+// GET "/" SERVE DATA OF ALL HANDYMEN (accessible to anyone visiting the site)
 router.get("/", async (_, res) => {
   const result = await services.getAllHandymen();
   return res.status(200).json(result);
 });
 
-// GET "/{id}"
+// GET "/{id}" SERVE DATA OF INDIVIDUAL HANDYMAN
 router.get("/:id", async (req, res) => {
   const result = await services.getHandymanById(parseInt(req.params.id));
   result ? res.status(200).send(result) : res.sendStatus(404);
 });
 
-// POST "/"
+// POST "/" ALLOW HANDYMAN DATA STORAGE
+// Note: this is used only during the initial stage of handyman registration process (accessible to anyone... 
+// ...who would like to rgister as handyman on the site)
 router.post("/", async (req, res) => {
   console.log(req.body)
   const result = await services.addNewHandyman(req.body);

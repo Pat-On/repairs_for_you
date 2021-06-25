d1b80dd29e8596820ade0b1e9aadd4ffcaf3844d
Michael <antenehnigussie1@yahoo.com>
2021-06-25 14:46:32 +0100

    Added more comment lines.

--- server/data/handymanRepository.js
+++ server/data/handymanRepository.js
@@ -7,11 +7,13 @@ function getAllHandymen() {
 }
 
 // FETCH DATA OF A HANDYMEN FROM DATABASE BY HANDYMAN ID (accessible to anyone visiting the site)
+// WARN: NOT CONNECTED TO DATABASE YET!
 function getHandymanById(hId) {
   return handymen.find((hMan) => hMan.id === hId);
 }
 
 // FETCH DATA OF ALL HANDYMEN FROM DATABASE BY HANDYMAN EMAIL
+// WARN: NOT CONNECTED TO DATABASE YET!
 // Note: this is used only during the initial stage of handyman registration process (accessible to anyone... 
 // ...who would like to rgister as handyman on the site)
 function getHandymanByEmail(email) {
@@ -19,6 +21,7 @@ function getHandymanByEmail(email) {
 }
 
 // STORE HANDYMAN DATA IN THE DATABASE (handypeople table)
+// WARN: NOT CONNECTED TO DATABASE YET!
 // Note: this is used only during the initial stage of handyman registration process (accessible to anyone... 
 // ...who would like to rgister as handyman on the site)
 function addNewHandyman(hData) {

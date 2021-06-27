ddbf59b0c3a69d8d9ad3c43fa6c120bf02e91279
SenaitEkubai <Senaitekubai@gmail.com>
2021-06-27 22:23:08 +0100

    moved alert to onchange function to apply for all buttons

--- client/src/components/adminPanel/AdminHandyPeopleTable.js
+++ client/src/components/adminPanel/AdminHandyPeopleTable.js
@@ -6,124 +6,125 @@ export default function AdminHandyPeopleTable(props) {
 	let { path, url } = useRouteMatch();
 
 
-  const [list, setList] = useState([]);
-  const [changed, setChanged] = useState(false);
+	const [list, setList] = useState([]);
+	const [changed, setChanged] = useState(false);
 
-  console.log(list);
+	console.log(list);
 
-  const handleChange = (e) => {
-    if (e.target.value === "Update") {
-      // setChanged(true);
-      props.history.push(`${path}/${e.target.id}`)
-    }
-    alert(`are you sure you want to ${e.target.value}`);
-  };
+	const handleChange = (e) => {
+		alert(`are you sure you want to ${e.target.value}`);
 
-  useEffect(() => {
-    // fetch("/api/users/handyman")
-    fetch("/api/users/handyman/adminsacceshandymans")
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error(res.statusText);
-        }
-        return res.json();
-      })
-      .then((body) => {
-        //!TODO: brutal solution of storing data inside the db as a string - fix it
-        const newArray = body.data.map((item) => {
-          return (item = {
-            ...item,
-            address_offer: JSON.parse(item.address_offer[0]),
-          });
-        });
-        console.log(newArray);
-        console.log(body.data[0].address_offer[0]);
-        setList(newArray);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
+		if (e.target.value === "Update") {
+			// setChanged(true);
+			props.history.push(`${path}/${e.target.id}`);
+		}
+	};
 
-  // const addressObj = {}
-  // if (list[0]) {
-  // console.log(JSON.parse(list[0].address_offer[0]));
-  // }
+	useEffect(() => {
+		// fetch("/api/users/handyman")
+		fetch("/api/users/handyman/adminsacceshandymans")
+			.then((res) => {
+				if (!res.ok) {
+					throw new Error(res.statusText);
+				}
+				return res.json();
+			})
+			.then((body) => {
+				//!TODO: brutal solution of storing data inside the db as a string - fix it
+				const newArray = body.data.map((item) => {
+					return (item = {
+						...item,
+						address_offer: JSON.parse(item.address_offer[0]),
+					});
+				});
+				console.log(newArray);
+				console.log(body.data[0].address_offer[0]);
+				setList(newArray);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, []);
 
-  // // Parsing address from DB
-  // const newArray = list.map(item => {
-  // 	return item = {
-  // 		...item,
-  // 		address_offer: JSON.parse(item.address_offer[0])
-  // 	}
-  // })
-  // setList(newArray)
+	// const addressObj = {}
+	// if (list[0]) {
+	// console.log(JSON.parse(list[0].address_offer[0]));
+	// }
 
-  return !changed ? (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>
-            {/* <input type="checkbox"></input> */}
-			<p>More</p>
-          </th>
-          <th scope="col">Id</th>
-          <th scope="col">First-name</th>
-          <th scope="col">Last-name</th>
-          <th scope="col">email</th>
-          <th scope="col">Telephone</th>
-          <th scope="col">Area</th>
-          <th scope="col">postcode</th>
-          <th scope="col">street-name</th>
-          <th scope="col">Joined</th>
-          <th scope="col">Jobs Done</th>
-          <th scope="col">status</th>
-          <th scope="col">Action</th>
-        </tr>
-      </thead>
-      {list.map((oneList, index) => (
-        <tbody key={index}>
-          <tr>
-            <td>
-				{/* add here link base on id uf user to the update profile */}
-              {/* <input type="checkbox"></input> */}
+	// // Parsing address from DB
+	// const newArray = list.map(item => {
+	// 	return item = {
+	// 		...item,
+	// 		address_offer: JSON.parse(item.address_offer[0])
+	// 	}
+	// })
+	// setList(newArray)
+
+	return !changed ? (
+		<table className="table">
+			<thead>
+				<tr>
+					<th>
+						{/* <input type="checkbox"></input> */}
+						<p>More</p>
+					</th>
+					<th scope="col">Id</th>
+					<th scope="col">First-name</th>
+					<th scope="col">Last-name</th>
+					<th scope="col">email</th>
+					<th scope="col">Telephone</th>
+					<th scope="col">Area</th>
+					<th scope="col">postcode</th>
+					<th scope="col">street-name</th>
+					<th scope="col">Joined</th>
+					<th scope="col">Jobs Done</th>
+					<th scope="col">status</th>
+					<th scope="col">Action</th>
+				</tr>
+			</thead>
+			{list.map((oneList, index) => (
+				<tbody key={index}>
+					<tr>
+						<td>
+							{/* add here link base on id uf user to the update profile */}
+							{/* <input type="checkbox"></input> */}
 			  <button>
-				<Link to={`${url}/${oneList.handyman_id}`}>View Repair Person</Link>
-			</button>
-            </td>
+								<Link to={`${url}/${oneList.handyman_id}`}>View Repair Person</Link>
+							</button>
+						</td>
 
-            <th scope="row">{oneList.handyman_id}</th>
-            <td>{oneList.first_name}</td>
-            <td>{oneList.last_name}</td>
+						<th scope="row">{oneList.handyman_id}</th>
+						<td>{oneList.first_name}</td>
+						<td>{oneList.last_name}</td>
 
-            <td>{oneList.email}</td>
-            <td>{oneList.phone_number}</td>
-            <td>{oneList.address_offer.city}</td>
-            <td>{oneList.postcode}</td>
-            <td>{oneList.address_offer.addressLineOne}</td>
-            <td>day/month/year</td>
-            {/* this will be the date the user was created  */}
-            <td>
+						<td>{oneList.email}</td>
+						<td>{oneList.phone_number}</td>
+						<td>{oneList.address_offer.city}</td>
+						<td>{oneList.postcode}</td>
+						<td>{oneList.address_offer.addressLineOne}</td>
+						<td>day/month/year</td>
+						{/* this will be the date the user was created  */}
+						<td>
               completed jobs
-              <br></br>
-              {/* also possible to make it into two columns */}
+							<br></br>
+							{/* also possible to make it into two columns */}
               inprogress jobs
-            </td>
-            <td>{oneList.visible ? "Visible" : "Hidden"}</td>
-            <td>
-              <select id={oneList.handyman_id} onChange={handleChange}>
-                <option>action</option>
-                <option>Update</option>
-                <option>Delete</option>
-                <option>Deactivate</option>
-                <option>Activate</option>
-              </select>
-            </td>
-          </tr>
-        </tbody>
-      ))}
-    </table>
-  ) : (
-    <UpdateForm />
-  );
+						</td>
+						<td>{oneList.visible ? "Visible" : "Hidden"}</td>
+						<td>
+							<select id={oneList.handyman_id} onChange={handleChange}>
+								<option>action</option>
+								<option>Update</option>
+								<option>Delete</option>
+								<option>Deactivate</option>
+								<option>Activate</option>
+							</select>
+						</td>
+					</tr>
+				</tbody>
+			))}
+		</table>
+	) : (
+		<UpdateForm />
+	);
 }

4119decbe7d0e17e3e7b41f2537c6a43983de713
patryk <patryk.r.nowak@gmail.com>
2021-06-29 19:27:12 +0100

    backbone of logic

--- client/src/containers/signIn/SignIn.js
+++ client/src/containers/signIn/SignIn.js
@@ -4,20 +4,16 @@ import { checkValidity } from "../../utility/utility";
 
 import AuthContext from "../../store/authContext";
 
-
 const SignIn = (props) => {
-
-	const authCtx = useContext(AuthContext);
-	//   const history = useHistory();
-	/**
+  const authCtx = useContext(AuthContext);
+  //   const history = useHistory();
+  /**
    * State: responsible for controlling the process of displaying the http req in UI
    */
 
-
   const [loadingControl, setLoadingControl] = useState(false);
 
-
-	/**
+  /**
    * State: signForm and setSignForm are used to provide:
    *  -information about requirements of validation
    *  -storing the input from user
@@ -37,13 +33,14 @@ const SignIn = (props) => {
       valid: true,
       // valid: false,
       touched: false,
+      incorrect: false,
     },
     password: {
       invalidInputInfo: "You have to input password",
       name: "Password",
       placeholder: "Password",
       value: "password",
-      objectConfig: {type: "password"},
+      objectConfig: { type: "password" },
       //!IMPORTANT detailed validation of password is not required now
       validation: {
         required: true,
@@ -52,6 +49,7 @@ const SignIn = (props) => {
       // valid: false,
       valid: true,
       touched: false,
+      incorrect: false,
     },
   });
 
@@ -62,91 +60,109 @@ const SignIn = (props) => {
    * @input no input
    * @return nothing
    */
-	const backFunction = () => {
-		//TODO: to home page
-		props.history.goBack();
-	};
+  const backFunction = () => {
+    //TODO: to home page
+    props.history.goBack();
+  };
 
-	/**
+  /**
    * @description Changing the page of form displayed in Browser
    * Going to next page
    * Function is changing the page by changing the state of loadingControl by setLoadingControl
    * @input no input
    * @return nothing
    */
-	const nextFunction = async () => {
-		try {
-			const data = {
-				email: signForm.email.value,
-				password: signForm.password.value,
-			};
+  const nextFunction = async () => {
+    try {
+      const data = {
+        email: signForm.email.value,
+        password: signForm.password.value,
+      };
 
-			const response = await fetch("/api/v1/users/login", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify(data),
-			});
+      const response = await fetch("/api/v1/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
-			const responseJsoned = await response.json();
-			console.log(responseJsoned)
-			authCtx.login(responseJsoned.token, responseJsoned.expirationTime);
-			props.history.push("/");
+      const responseJsoned = await response.json();
+      console.log(responseJsoned);
+      if (responseJsoned.token) {
+        authCtx.login(responseJsoned.token, responseJsoned.expirationTime);
+        props.history.push("/");
+      }
+      if (responseJsoned.status === "fail") {
+        //TODO: automate process
+        setSignForm((prevState) => {
+          return {
+            ...prevState,
+            email: {
+              ...prevState.email,
+			  incorrect: true,
+            },
+			password: {
+				...prevState.password,
+				incorrect: true,
+			}
+          };
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
 
-		} catch (error) {
-			console.log(error);
-		}
-
-		// // !TODO: if logged success main page + auth if not error -> wrong password or email
-		// props.history.replace("/");
-	};
-
-	/**
+    // // !TODO: if logged success main page + auth if not error -> wrong password or email
+    // props.history.replace("/");
+  };
+  console.log(signForm.email.incorrect);
+  /**
    * @DescriptionFunction is changing the value, touched and valid stored inside the signForm
    * function is using the helper function checkValidity(), which is evaluating upcoming input
    * @param {Object} e [e - event object triggered by input provided by user]
    * @param {String} formName [Is a name of the input's field in form, used to identify what should be updated]
    */
-	const inputChangeHandler = (e, formName) => {
-		const updatedForm = {
-			...signForm,
-			[formName]: {
-				...signForm[formName],
-				value: e.target.value,
-				touched: true,
-				valid: checkValidity(e.target.value, signForm[formName].validation),
-			},
-		};
-		setSignForm(updatedForm);
-	};
+  const inputChangeHandler = (e, formName) => {
+    const updatedForm = {
+      ...signForm,
+      [formName]: {
+        ...signForm[formName],
+        value: e.target.value,
+        touched: true,
+        valid: checkValidity(e.target.value, signForm[formName].validation),
+      },
+    };
+    setSignForm(updatedForm);
+  };
 
-	/**
+  /**
    * For Loop which is creating the array of the object containing the id and needed information
    * in a process of rendering the form's pages within switch statement
    */
-	const formElementKeyArray = [];
-	for (let key in signForm) {
-		formElementKeyArray.push({
-			id: key,
-			config: signForm[key],
-		});
-	}
+  const formElementKeyArray = [];
+  for (let key in signForm) {
+    formElementKeyArray.push({
+      id: key,
+      config: signForm[key],
+    });
+  }
 
-	const form = (
-		<SignUpForm
-			form={formElementKeyArray}
-			formInputHandler={inputChangeHandler}
-			next={nextFunction}
-			back={backFunction}
-			buttonDisable={!(signForm.password.valid && signForm.email.valid)}
-			nameOfTheForm="Sign-In"
-			leftButtonName="Cancel"
-			rightButtonName="Log In"
-		/>
-	);
+  const form = (
+    <SignUpForm
+      form={formElementKeyArray}
+      formInputHandler={inputChangeHandler}
+      next={nextFunction}
+      back={backFunction}
+      buttonDisable={!(signForm.password.valid && signForm.email.valid)}
+      nameOfTheForm="Sign-In"
+      leftButtonName="Cancel"
+      rightButtonName="Log In"
+	  wrongPasswordEmail={!(signForm.password.incorrect && signForm.email.incorrect)}
+    />
+  );
 
-	return <div>{form}</div>;
+  return <div>{form}</div>;
 };
 
 export default SignIn;

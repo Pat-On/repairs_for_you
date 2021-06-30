25649b80dae0f156ccf69bcb02c738e046b17978
patryk <patryk.r.nowak@gmail.com>
2021-06-30 16:30:29 +0100

    save

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import { Route, Switch, Redirect, withRouter } from "react-router-dom";
 import React, { useContext, Suspense } from "react";
 
-import classes from  "./App.module.scss";
+import classes from "./App.module.scss";
 import Layout from "./hoc/Layout/Layout";
 import Login from "./containers/signUp/SignUp";
 import MainPage from "./components/mainPage/mainPage";
 import SignOut from "./containers/signOut/signOut";
 import AuthContext from "./store/authContext";
-import Spinner from "./UI/Spinner/Spinner"
+import Spinner from "./UI/Spinner/Spinner";
 
 //Lazy loading
 const AdminPanel = React.lazy(() => {
@@ -23,63 +23,71 @@ const SignIn = React.lazy(() => {
   return import("./containers/signIn/SignIn");
 });
 
-
-
 const App = () => {
   const authCtx = useContext(AuthContext);
 
   return (
     <div className={classes.container}>
       <Layout>
-        <Suspense fallback={<div className={classes.spinner__container}><Spinner/></div>}>
-        <Switch>
-          <Route
-            path="/"
-            exact
-            component={(props) => <MainPage {...props} />}
-          />
-          <Route
-            path="/buyers"
-            component={() => (
-              <div>
-                <h1>PLACEHOLDER buyers</h1>
-              </div>
-            )}
-          />
-
-          <Route
-            path="/users/handyman"
-            render={(props) => <HandymanRoutes {...props} />}
-          />
-
-          <Route
-            path="/contact"
-            render={() => (
-              <div>
-                <h1>
-                  <Contact />{" "}
-                </h1>
-              </div>
-            )}
-          />
-
-          <Route
-            path="/signinout"
-            component={(props) => <SignOut {...props} />}
-          />
-          <Route path="/signin" component={(props) => <SignIn {...props} />} />
-
-          {authCtx.isLoggedIn && (
+        <Suspense
+          className={classes.container__test}
+          fallback={
+            <div className={classes.spinner__container}>
+              <Spinner />
+            </div>
+          }
+        >
+          <Switch>
             <Route
-              path="/admin-panel"
-              render={(props) => <AdminPanel {...props} />}
+              path="/"
+              exact
+              component={(props) => <MainPage {...props} />}
             />
-          )}
+            <Route
+              path="/buyers"
+              component={() => (
+                <div>
+                  <h1>PLACEHOLDER buyers</h1>
+                </div>
+              )}
+            />
 
-          <Route path="/login" component={(props) => <Login {...props} />} />
+            <Route
+              path="/users/handyman"
+              render={(props) => <HandymanRoutes {...props} />}
+            />
 
-          <Redirect to="/" />
-        </Switch>
+            <Route
+              path="/contact"
+              render={() => (
+                <div>
+                  <h1>
+                    <Contact />{" "}
+                  </h1>
+                </div>
+              )}
+            />
+
+            <Route
+              path="/signinout"
+              component={(props) => <SignOut {...props} />}
+            />
+            <Route
+              path="/signin"
+              component={(props) => <SignIn {...props} />}
+            />
+
+            {authCtx.isLoggedIn && (
+              <Route
+                path="/admin-panel"
+                render={(props) => <AdminPanel {...props} />}
+              />
+            )}
+
+            <Route path="/login" component={(props) => <Login {...props} />} />
+
+            <Redirect to="/" />
+          </Switch>
         </Suspense>
       </Layout>
     </div>

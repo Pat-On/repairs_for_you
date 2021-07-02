228c489bbcec80e4ba064f580cb58c5172bff26b
patryk <patryk.r.nowak@gmail.com>
2021-07-02 14:18:55 +0100

    added comment in controller

--- server/controller/handymanController.js
+++ server/controller/handymanController.js
@@ -1,5 +1,12 @@
 import handymanModel from "../model/handymanModel";
 
+
+/**
+ * @description controller what is controlling the process of sending three random handyman stored in DB
+ * @param {object request from express} req 
+ * @param {object response from express} res 
+ * @param {express next method dedicated to the middleware function} next 
+ */
 exports.threeRandomHandyman = async (req, res, next) => {
   try {
     const threeRandomHandyman = await handymanModel.threeRandomHandyman();

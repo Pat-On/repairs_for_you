e88653a912047c02585d5691622aa7621e76eac1
patryk <patryk.r.nowak@gmail.com>
2021-07-02 14:24:21 +0100

    deleted all what is not needed

--- client/src/components/mainPage/customersSection/CustomerSection.js
+++ client/src/components/mainPage/customersSection/CustomerSection.js
@@ -4,10 +4,7 @@ import classes from "./CustomerSection.module.scss";
 import backgroundVideo from "../../../public/ConstructionWorkersDESKTOP.mp4";
 import CustomerCommentMain from "./customerCommentMain/CustomerCommentMain";
 
-import customer1 from "../../../public/5.jpg";
-import customer2 from "../../../public/17.jpg";
-import customer3 from "../../../public/81.jpg";
-import { useEffect } from "react";
+import Spinner from "../../../UI/Spinner/Spinner"
 
 /**
  * @DescriptionFunction Sub component used in MainPage component
@@ -16,42 +13,9 @@ const CustomerSection = (props) => {
   const [handymenProfiles, setHandymanProfiles] = useState([]);
   useEffect(() => {}, []);
 
-  // !TODO: HARD CODED CUSTOMER next step randomize comments from DB
-  // const commentArray = [
-  //   {
-  //     photo: customer1,
-  //     comment: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-  //     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-  //     exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-  //      in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
-  //     review: 5,
-  //     user_name: "Name1",
-  //     user_surname: "Surname1",
-  //   },
-  //   {
-  //     photo: customer2,
-  //     comment: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-  //   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-  //   exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-  //    in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
-  //     review: 3,
-  //     user_name: "Name2",
-  //     user_surname: "Surname2",
-  //   },
-  //   {
-  //     photo: customer3,
-  //     comment: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-  // tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-  // exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-  //  in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
-  //     review: 4,
-  //     user_name: "Name3",
-  //     user_surname: "Surname3",
-  //   },
-  // ];
 
   //!TODO: proper spinner
-  let usersComments = <p> LOADING... </p>;
+  let usersComments = <Spinner />
 
   if (commentArray.length === 3) {
     usersComments = commentArray.map((item) => (
